4223d3b5110387b8a027321fc9f3833d84f0a293
manumn95 <manumnofficial.95@gmail.com>
2024-07-03 17:42:59 +0530

    Added fridge in product

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { LuUserCircle2 } from "react-icons/lu";
+import { FaShoppingCart } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header className="h-16 shadow-md">
+      <div className="h-full container mx-auto flex items-center justify-between px-4">
+        <div className="">
+          <Logo w={90} h={50}></Logo>
+        </div>
+
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
+          <input
+            type="search"
+            placeholder="search product here..."
+            className="w-full outline-none "
+          ></input>
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+            <GrSearch />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-7">
+          <div className="text-3xl cursor-pointer">
+            <LuUserCircle2 />
+          </div>
+          <div className="text-2xl relative">
+            <span>
+              
+              <FaShoppingCart />
+            </span>
+            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+
+          <div>
+            <button className="px-3 py-1 rounded-full bg-red-600 hover:bg-red-700 text-white">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
