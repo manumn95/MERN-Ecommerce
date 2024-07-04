071fdfd606cd69272a21cc89013872e4ccb80589
manumn95 <manumnofficial.95@gmail.com>
2024-07-04 13:42:10 +0530

    Added login functionality

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 const router = require('./routes/index')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api',router);
