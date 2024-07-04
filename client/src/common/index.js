bdc8b2d6bc45cec0c580b927480f978cf5dbf257
manumn95 <manumnofficial.95@gmail.com>
2024-07-04 12:16:00 +0530

    Added signup funcionality

---
+++ client/src/common/index.js
@@ -0,0 +1,7 @@
+const backendDomain='http://localhost:8000'
+const summaryApi = {
+  signUp:{
+    url:`${backendDomain}/api/signUp`,
+  }
+}
+export default summaryApi;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,19 +1,24 @@
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import login_icon from "../assets/signin.gif";
 import { imageToBase64 } from "../helpers/imageToBase64";
+import axios from "axios";
+import summaryApi from "../common";
+import { toast } from "react-toastify";
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const[showConfirmPassword,setConfirmPassword] = useState(false)
+  const [showConfirmPassword, setConfirmPassword] = useState(false);
   const [data, setData] = useState({
-    name:"",
+    name: "",
     email: "",
     password: "",
-    confirmPassword:"",
-    profilePic:""
+    confirmPassword: "",
+    profilePic: "",
   });
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,43 +31,61 @@ const Signup = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (data.password === data.confirmPassword) {
+      const dataResponse = await axios.post(summaryApi.signUp.url, data);
+    if(dataResponse.data.success)
+      {
+        toast.success(dataResponse.data.message);
+        navigate('/login')
+      }
+      if(dataResponse.data.error)
+        {
+          toast.error(dataResponse.data.message);
+        }
+
+    } else {
+      console.log("Please check the password");
+    }
   };
 
-  const handleUploadPic=async (e)=>{
+  const handleUploadPic = async (e) => {
     const file = e.target.files[0];
-    const imagePic = await imageToBase64(file)
-    setData((prev)=>{
-      return{
+    const imagePic = await imageToBase64(file);
+    setData((prev) => {
+      return {
         ...prev,
-        profilePic:imagePic
-      }
-    })
-  }
+        profilePic: imagePic,
+      };
+    });
+  };
 
   return (
     <section id="signup">
       <div className="mx-auto container p-4">
         <div className="bg-white p-5 w-full max-w-sm mx-auto">
           <div className="w-20 h-20 mx-auto relative overflow-hidden rounded-full">
-           <div>
-           <img src={data.profilePic || login_icon} alt="login-icon"></img>
-           </div>
-        <form>
-          <label>
-          <div className="text-sm bg-opacity-50 bg-slate-200 py-3 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full">
-            upload photo
-           </div>
-            <input className="hidden" type="file" onChange={handleUploadPic}></input>
-          </label>
-      
-        </form>
+            <div>
+              <img src={data.profilePic || login_icon} alt="login-icon"></img>
+            </div>
+            <form>
+              <label>
+                <div className="text-sm bg-opacity-50 bg-slate-200 py-3 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full">
+                  upload photo
+                </div>
+                <input
+                  className="hidden"
+                  type="file"
+                  onChange={handleUploadPic}
+                ></input>
+              </label>
+            </form>
           </div>
 
           <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
-
-          <div className="grid">
+            <div className="grid">
               <label>Name:</label>
               <div className="bg-slate-200 p-2">
                 <input
@@ -109,7 +132,6 @@ const Signup = () => {
                   <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
                 </div>
               </div>
-           
             </div>
 
             <div className="grid">
@@ -128,18 +150,19 @@ const Signup = () => {
                   className="cursor-pointer text-xl"
                   onClick={() => setConfirmPassword(!showConfirmPassword)}
                 >
-                  <span>{showConfirmPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  <span>
+                    {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
                 </div>
               </div>
-             
             </div>
 
             <button className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-allall mx-auto block mt-6 hover:bg-red-700">
-             SignUp
+              SignUp
             </button>
           </form>
           <p className="my-5 text-red-600 hover:text-red-700 hover:underline">
-           Already have account ? <Link to={"/login"}>Login</Link>
+            Already have account ? <Link to={"/login"}>Login</Link>
           </p>
         </div>
       </div>
