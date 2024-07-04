7a16aeedc7f6728a076a5898cbe482921d388413
manumn95 <manumnofficial.95@gmail.com>
2024-07-04 16:13:59 +0530

    Added logout functionality

---
+++ server/controllers/userDetail.js
@@ -0,0 +1,20 @@
+const userModel = require("../models/userModel");
+
+const userDetailController =async (req, res) => {
+  try {
+   const user = await userModel.findById(req.userId);
+   res.status(200).json({
+    data:user,
+    error:false,
+    success:true,
+    message:"Login User-Detail"
+   })
+  } catch (err) {
+    res.status(400).json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
+};
+module.exports = userDetailController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,13 @@ const router = express.Router();
 
 const userSignUpController = require('../controllers/userSignUp');
 const userSignIn = require('../controllers/userSIgnin');
+const userDetailController = require('../controllers/userDetail');
+const authToken = require('../middleware/authToken,js');
+const userLogOut = require('../controllers/userLogout');
 
 router.post('/signUp',userSignUpController)
 router.post('/signIn',userSignIn);
+router.get('/user-details',authToken, userDetailController)
+router.get('/user-logout',userLogOut);
 
 module.exports = router;
