507697537ecc859462343d6b9c545e1d86c67917
manumn95 <manumnofficial.95@gmail.com>
2024-07-12 22:48:00 +0530

    Added all order api

--- server/controllers/product/filterProduct.js
+++ server/controllers/product/filterProduct.js
@@ -1,27 +1,29 @@
-const productModel = require('../../models/productModel');
+const productModel = require("../../models/productModel")
 
-const filterProductController = async (req, res) => {
-  try {
-    const categoryList = req?.body?.category || [];
+const filterProductController = async(req,res)=>{
+ try{
+        const categoryList = req?.body?.category || []
 
-    // Build the query based on the category list
-    const query = categoryList.length > 0 ? { category: { "$in": categoryList } } : {};
+        const product = await productModel.find({
+            category :  {
+                "$in" : categoryList
+            }
+        })
 
-    const products = await productModel.find(query);
-
+        res.json({
+            data : product,
+            message : "product",
+            error : false,
+            success : true
+        })
+ }catch(err){
     res.json({
-      data: products,
-      message: "Filter Product",
-      error: false,
-      success: true
-    });
-  } catch (err) {
-    res.json({
-      message: err.message || err,
-      success: false,
-      error: true
-    });
-  }
-};
+        message : err.message || err,
+        error : true,
+        success : false
+    })
+ }
+}
 
-module.exports = filterProductController;
+
+module.exports = filterProductController
