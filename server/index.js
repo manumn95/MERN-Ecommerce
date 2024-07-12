3e348973400aee1624f2384fb6ab6c090a86f7a9
manumn95 <manumnofficial.95@gmail.com>
2024-07-12 17:14:17 +0530

    vercel config file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://mern-ecommerce-frontend-amber.vercel.app/',
+    origin: 'https://mern-ecommerce-frontend-amber.vercel.app',
     credentials: true,
   })
 );
