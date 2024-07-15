fc12c5346286db5bff1504929a078dc2f265d024
manumn95 <manumnofficial.95@gmail.com>
2024-07-15 11:37:04 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors({
   credentials : true
 }))
 
-app.use(express.json({ limit: '50mb' }));  
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '150mb' }));  
+app.use(express.urlencoded({ limit: '150mb', extended: true }));
 
 app.use(cookieParser());
 app.use("/api", router);
